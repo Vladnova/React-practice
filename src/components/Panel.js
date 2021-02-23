34046d293d5724e5aa086546dcc338ea754721a1
Vladislav Novalenko <novalenkov@ukr.net>
2021-02-24 00:42:45 +0200

    finish les-1

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,12 +1,29 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-
+const styles = {
+    container:{
+        fontSize:24,
+        color:'red',
+    }
+}
 const Panel = ({title, children}) => (
-    <div>
+    <div style={styles.container}>
         {title && <h2>{title}</h2>}
         {children}         
     </div>
     
 );
+
+Panel.defaulrProps = {
+    title: "",
+    children: [],
+};
+
+Panel.propTypes = {
+    title: PropTypes.string,
+    children: PropTypes.node,
+};
+
     
 export default Panel;
